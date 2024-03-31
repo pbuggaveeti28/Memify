fdfb9c4d9bcc7faafb1b3ad24ad48d56fbf62669
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-03-30 20:51:37 -0700

    Added classes and styling to meme.js

--- react-app/src/Meme.js
+++ react-app/src/Meme.js
@@ -2,15 +2,13 @@ import React from "react";
 
 function Meme() {
   return (
-    <form action="/submit-form" method="post">
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="name" required />
-
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required />
-
-      <input type="submit" value="Submit" />
-    </form>
+    <main>
+      <form className="form">
+        <input type="text" className="form-input" placeholder="Top text" />
+        <input type="text" className="form-input" placeholder="Bottom text" />
+        <button className="form-button">Get a new meme🖼️</button>
+      </form>
+    </main>
   );
 }
 
