0be2d858ea1ac23b2e74004fc429eae82e37d420
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-04-02 00:03:35 -0700

    Added random property

--- react-app/src/Meme.js
+++ react-app/src/Meme.js
@@ -1,14 +1,25 @@
 import React from "react";
+import memeData from "./memeData";
 
 function Meme() {
+  let url;
+  function getMeme() {
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    url = memesArray[randomNumber].url;
+    console.log(url);
+  }
   return (
     <main>
+      <p>{url}</p>
       <div className="form">
         <input type="text" className="form-input" placeholder="Shut up" />
 
         <input type="text" className="form-input" placeholder="take my money" />
 
-        <button className="form-button">Get a new meme🖼️</button>
+        <button className="form-button" onClick={getMeme}>
+          Get a new meme🖼️
+        </button>
       </div>
     </main>
   );
