5f0765016cd811ecce3307e191eb0532ea5dab59
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-04-13 17:36:50 -0700

    Update Meme.js

--- react-app/src/Meme.js
+++ react-app/src/Meme.js
@@ -18,18 +18,44 @@ export default function Meme() {
       randomImage: url,
     }));
   }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
   return (
     <main>
       <div className="form">
-        <input type="text" className="form-input" placeholder="Shut up" />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Top text"
+          value={meme.topText}
+          onChange={handleChange}
+          name="topText"
+        />
 
-        <input type="text" className="form-input" placeholder="take my money" />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
 
         <button className="form-button" onClick={getMeme}>
           Get a new meme🖼️
         </button>
       </div>
-      <img src={meme.randomImage} className="meme-image" />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme-image" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
