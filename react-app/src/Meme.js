cf4bcb7f3ce1cdf0c6ddda856e57a6c364406570
Padmaja Buggaveeti <59131010+pbuggaveeti28@users.noreply.github.com>
2024-04-13 19:37:52 -0700

    Fixed classNames

--- react-app/src/Meme.js
+++ react-app/src/Meme.js
@@ -40,7 +40,7 @@ export default function Meme() {
         <input
           type="text"
           placeholder="Top text"
-          className="form--input"
+          className="form-input"
           name="topText"
           value={meme.topText}
           onChange={handleChange}
@@ -48,19 +48,19 @@ export default function Meme() {
         <input
           type="text"
           placeholder="Bottom text"
-          className="form--input"
+          className="form-input"
           name="bottomText"
           value={meme.bottomText}
           onChange={handleChange}
         />
-        <button className="form--button" onClick={getMemeImage}>
+        <button className="form-button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
       <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
-        <h2 className="meme--text top">{meme.topText}</h2>
-        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        <img src={meme.randomImage} className="meme-image" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
